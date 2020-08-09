821dc656dcedca90444fe7ce14f91490f0c9b594
Omar Amoako <>
2020-08-09 19:40:34 +0100

    Created sequalize models

--- app.js
+++ app.js
@@ -15,7 +15,24 @@ const Belts = sequelize.define('Belts', {
 	id: {
 		field: 'id',
 		type: Sequelize.INTEGER,
+	}, 
+	name: {
+		field: 'name',
+		type: Sequelize.STRING,
+	}, 
+	organizationId: {
+		field: 'organization_id',
+		type: Sequelize.INTEGER,
 		primaryKey: true
+	}, 
+	weightClassId: {
+		field: 'weightClass_id',
+		type: Sequelize.INTEGER,
+		primaryKey: true
+	}, 
+	boxerId: {
+		field: 'boxer_id',
+		type: Sequelize.INTEGER,
 	}
 }, {
 		timestamps: false
@@ -26,6 +43,103 @@ const Organization = sequelize.define('Organization', {
 		field: 'id',
 		type: Sequelize.INTEGER,
 		primaryKey: true
+	},
+	shortName: {
+		field: 'shortName',
+		type: Sequelize.STRING,
+	},
+	fullName: {
+		field: 'fullName',
+		type: Sequelize.STRING,
+	}
+}, {
+		freezeTableName: true,
+		timestamps: false
+})
+
+const Boxers = sequelize.define('Boxers', {
+	id: {
+		field: 'id',
+		type: Sequelize.INTEGER,
+		primaryKey: true
+	},
+	firstName: {
+		field: 'firstName',
+		type: Sequelize.STRING,
+	},
+	lastName: {
+		field: 'lastName',
+		type: Sequelize.STRING,
+	},
+	dateOfBirth: {
+		field: 'dateOfBirth',
+		type: Sequelize.STRING,
+	},
+	weightClassId: {
+		field: 'weight_id',
+		type: Sequelize.INTEGER,
+	},
+	thumbnailUrl: {
+		field: 'thumbnailUrl',
+		type: Sequelize.STRING,
+	}
+}, {
+		freezeTableName: true,
+		timestamps: false
+})
+
+const Events = sequelize.define('Events', {
+	id: {
+		field: 'id',
+		type: Sequelize.INTEGER,
+		primaryKey: true
+	},
+	title: {
+		field: 'title',
+		type: Sequelize.STRING,
+	},
+	date: {
+		field: 'date',
+		type: Sequelize.STRING,
+	},
+	venue: {
+		field: 'venue',
+		type: Sequelize.STRING,
+	},
+	country: {
+		field: 'country',
+		type: Sequelize.STRING,
+	},
+	weightClassId: {
+		field: 'weight_id',
+		type: Sequelize.INTEGER,
+	},
+	mainEventId: {
+		field: 'mainEvent_id',
+		type: Sequelize.INTEGER,
+	},
+	boxer1Id: {
+		field: 'boxer1_id',
+		type: Sequelize.INTEGER,
+	},
+	boxer2Id: {
+		field: 'boxer2_id',
+		type: Sequelize.INTEGER,
+	}
+}, {
+		freezeTableName: true,
+		timestamps: false
+})
+
+const WeightClass = sequelize.define('WeightClass', {
+	id: {
+		field: 'id',
+		type: Sequelize.INTEGER,
+		primaryKey: true
+	},
+	weight: {
+		field: 'weight',
+		type: Sequelize.STRING,
 	}
 }, {
 		freezeTableName: true,
@@ -40,12 +154,17 @@ app.get('/user/:id', (req, res) => {
 		results["belts"] = belts
 	}).then( Organization.findAll().then( (org) => {
 		results["organizations"] = org
-		console.log(results["belts"])
+	})).then(Boxers.findAll().then( (boxer) => {
+		results["boxers"] = boxer
+	})).then(Events.findAll().then( (event) => {
+		results["events"] = event
+	})).then(WeightClass.findAll().then( (weight) => {
+		results["weight"] = weight
 		res.json(results)
-	})
-	)
-	//console.log(results)
+	}))
+	//console.log(results["belts"])
 })
+
 
 app.get("/", (req, res) => {
 	console.log("Respoding to route")
